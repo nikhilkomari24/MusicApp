f1143ddab9aa52bb4aad192f8c45b1f4aa31bc80
nkomari <55295181+nkomari@users.noreply.github.com>
2019-12-03 21:49:28 -0500

    added more secure routes and added secure methods

---
+++ backend_node/controllers/secure.controller.js
@@ -0,0 +1,187 @@
+const Song = require('../models/song.model.js');
+const Review = require('../models/review.model.js');
+const Rating = require('../models/ratings.model.js');
+const Playlist = require('../models/playlist.model.js');
+
+//need to change this here and everywhere
+function generateKeyValueFromBody(body) {
+    const entries = Object.keys(body)
+    const inserts = {}
+    for (let i = 0; i < entries.length; i++) {
+        inserts[entries[i]] = Object.values(body)[i]
+    }
+    return inserts;
+}
+
+//adding a new song by user
+exports.addbyuser = (req, res) => {
+    const inserts = generateKeyValueFromBody(req.body)
+    Song.create(inserts)
+        .then(data => {
+            if (Boolean(data["_id"])) {
+                res.status(200).send({ message: data["_id"] })
+            }
+            else {
+                // didnt insert 
+                res.status(500).send({ message: "false" })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while adding a new song by user."
+            })
+        });
+
+};
+
+// adding a rating by user
+exports.addrating = (req, res) => {
+    const inserts = generateKeyValueFromBody(req.body)
+    var songID = inserts["songId"]
+    Rating.create(inserts)
+        .then(data => {
+            if (Boolean(data["_id"])) {
+                res.status(200).send({ message: "true" })
+            }
+            else {
+                // didnt insert 
+                res.status(500).send({ message: songID })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: songID
+            })
+        });
+};
+// adding a review by user
+exports.addreview = (req, res) => {
+    const inserts = generateKeyValueFromBody(req.body)
+    var songID = inserts["songId"]
+    Review.create(inserts)
+        .then(data => {
+            if (Boolean(data["_id"])) {
+                res.status(200).send({ message: "true" })
+            }
+            else {
+                res.status(500).send({ message: songID })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: songID })
+        });
+};
+
+// creating a playlist by user
+exports.createpl = (req, res) => {
+
+    const inserts = generateKeyValueFromBody(req.body)
+
+    Playlist.create(inserts)
+        .then(data => {
+            if (Boolean(data["_id"])) {
+                res.status(200).send({ message: data["_id"] })
+            }
+            else {
+                // didnt insert 
+                res.status(500).send({ message: "false" })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating a new playlist by user."
+            })
+        });
+};
+
+//editing of a song title and desc by user
+exports.editpl = (req, res) => {
+    const updates = generateKeyValueFromBody(req.body)
+    var playlistid = updates.playListID
+    var ownerid = updates.ownerID
+
+    delete updates.playListID
+    delete updates.ownerID
+
+    Playlist.updateOne({ _id: playlistid, ownerid: ownerid }, { $set: updates })
+        .then(data => {
+            if (Boolean(data["nModified"])) {
+
+                res.status(200).send({ message: "success" })
+            }
+            else {
+                res.status(200).send({ message: "false" })
+            }
+
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while editing playlist title and desc by user."
+            })
+        });
+};
+
+exports.addsongpl = (req, res) => {
+    var playlistid = req.body.playListID
+    var songid = req.body.songID
+    var ownerid = req.body.ownerID
+
+    Playlist.updateOne({ _id: playlistid, ownerid: ownerid }, { "$push": { "songid": songid } })
+        //.then(data=>res.send(data))
+        .then(data => {
+            if (Boolean(data["nModified"])) {
+                res.status(200).send({ message: "true" })
+            }
+            else {
+                // didnt insert 
+                res.status(500).send({ message: "false" })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while adding a song to playlist by user."
+            })
+        });
+};
+
+exports.remsongpl = (req, res) => {
+    var playlistid = req.body.playListID
+    var songid = req.body.songID
+    var ownerid = req.body.ownerID
+
+    Playlist.updateOne({ _id: playlistid, ownerid: ownerid }, { "$pull": { "songid": songid } })
+        .then(data => {
+            if (Boolean(data["nModified"])) {
+                res.status(200).send({ message: "true" })
+            }
+            else { 
+                res.status(500).send({ message: "false" })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while removing a song from playlist by user."
+            })
+        });
+};
+
+exports.hidepl = (req, res) => {
+    var playlistid = req.body.playListID
+    var hidden = req.body.hidden
+    var ownerid = req.body.ownerID
+    Playlist.updateOne({ _id: playlistid, ownerid: ownerid }, { $set: { hidden: hidden } })
+        .then(data => {
+            if (Boolean(data["nModified"])) {
+                res.status(200).send({ message: "true" })
+            }
+            else {
+                // didnt insert 
+                res.status(500).send({ message: "false" })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while hiding a playlist by user."
+            })
+        });
+};
--- backend_node/routes/secure.route.js
+++ backend_node/routes/secure.route.js
@@ -2,12 +2,27 @@ module.exports = (app) => {
     const secure = require('../controllers/secure.controller.js');
 
     // adding a new song
-    app.post('/secure/addsong', secure.add);
+    app.post('/secure/addsong', secure.addbyuser);
 
     // adding a rating
     app.post('/secure/addrating', secure.addrating);
 
     // adding a review
-    app.post('/secure/addreview', secure.addreview);    
+    app.post('/secure/addreview', secure.addreview);
+
+    // creating a playlist
+    app.post('/secure/createplaylist', secure.createpl);
+
+    //changing title and description only by owner
+    app.put('/secure/editplaylist', secure.editpl);
+
+    //adding a song to playlist
+    app.put('/secure/playlist/addsong', secure.addsongpl);
+
+    //removing a song from playlist
+    app.delete('/secure/playlist/removesong', secure.remsongpl);
+
+    // hiding a playlist
+    app.put('/secure/hideplaylist', secure.hidepl);
 
 }
